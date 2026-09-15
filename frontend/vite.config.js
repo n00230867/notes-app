53f12a8f70a1fcfd7610056ce4109e78f73f5ed2
Eduards Oss (Student) <N00230867@iadt.ie>
2026-09-15 11:45:16 +0100

    Stretch goals

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": "http://localhost:8000",
-    },
+    // proxy: {
+    //   "/api": "http://localhost:8000",
+    // },
   },
 });
